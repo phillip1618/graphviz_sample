97e633f7bd9f6011b8f403fab6e136c0106b30e1
Phillip Kim <phillipkakg@gmail.com>
2026-07-01 14:51:45 -0400

    edit

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-import numpy as np
-import networkx as nx
 import graphviz
-import scipy.io as sio
 import networkx as nx
+import scipy.io as sio
 
+from constant import MTX_FILE_NAME
 
-MTX_FILE_NAME = 'Alemdar.mtx'
 
 matrix = sio.mmread(MTX_FILE_NAME)
 
@@ -13,6 +11,9 @@ G = nx.from_scipy_sparse_array(matrix)
 
 dot = nx.nx_agraph.to_agraph(G)
 dot.graph_attr.update(overlap="false", splines="true")
+
+print('Number of edges: ', dot.number_of_edges())
+print('Number of nodes: ', dot.number_of_nodes())
 
 sfdp_graph = graphviz.Source(dot.string(), format='png', engine='sfdp')
 
